e03da8a50c4a54f36536ebea5c7c842b7ab72bb7
kagmanoj25 <31121876+kagmanoj25@users.noreply.github.com>
2018-07-04 10:42:04 +0530

    Update NavigatorViewContainer.js

--- src/modules/navigator/NavigatorViewContainer.js
+++ src/modules/navigator/NavigatorViewContainer.js
@@ -2,7 +2,7 @@ import {connect} from 'react-redux';
 import NavigatorView from './NavigatorView';
 
 export default connect(
-  state => ({
-    navigatorState: state.get('navigatorState').toJS()
-  })
+//   state => ({
+//     navigatorState: state.get('navigatorState').toJS()
+//   })
 )(NavigatorView);
